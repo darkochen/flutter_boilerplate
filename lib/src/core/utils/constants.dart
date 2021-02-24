b4bf4f812e4bd192f6ec8c96c5d37a8cb0136e47
darko.mt.chen <darko.chen@gmail.com>
2021-02-23 21:40:24 -0800

    use real news API key

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -3,7 +3,7 @@ const String kMaterialAppTitle = 'Flutter Clean Architecture';
 // API
 const String kBaseUrl = 'https://newsapi.org/v2';
 
-const String kApiKey = 'ff957763c54c44d8b00e5e082bc76cb0';
+const String kApiKey = '39e9cb57394441e1816651a1b511140a';
 
 // Database
 const String kArticlesTableName = 'articles_table';
